bd5d0077e78087fd1cc9b47708fde9ea82580d86
amany091 <amanyzeyada10@gmail.com>
2024-09-19 19:36:12 +0300

    add card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
+import Card from './components/card/Card'
 
 export default function App() {
   return (
-   <div></div>
+    <div>
+      <Card title="t-shirt" price="30$" rating="3.5" discount={14} />
+   </div>
   )
 }
 
